147a387f183a6edb179afe452c654c1fd1c0b45d
icecoldfinishing <rohyamboara@gmail.com>
2026-01-28 20:29:52 +0300

    ok

--- project/src/main/java/etu/sprint/controller/HelloController.java
+++ project/src/main/java/etu/sprint/controller/HelloController.java
@@ -2,12 +2,17 @@ package etu.sprint.controller;
 
 import etu.sprint.annotation.AnnotationController;
 import etu.sprint.annotation.GetMapping;
+import etu.sprint.model.ModelView;
+
 
 @AnnotationController("/")
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
-        return "<h1>Hello World from Annotation Controller!</h1>";
+    public ModelView hello() {
+        ModelView mv = new ModelView();
+        mv.addItem("message", "Hello World!");
+        mv.setView("/views/hello.jsp");
+        return mv;
     }
 }
